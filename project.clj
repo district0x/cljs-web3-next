3e4d4205831995b220d75b55cf9691705f89a1f2
filip <fbielejec@gmail.com>
2019-10-24 21:21:02 +0200

    use prfi

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-web3-next "0.0.9"
+(defproject cljs-web3-next "0.0.9-SNAPSHOT"
   :description "ClojureScript Web3 library with swappable backends."
   :url "https://github.com/district0x/cljs-web3-next"
   :license {:name "Eclipse Public License"
