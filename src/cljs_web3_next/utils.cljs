320a34dfc6e114e40d9f07db9a4236163feb28d0
x <x@xs-MacBook-Pro.local>
2021-09-15 11:08:14 +0300

    Translate evm and refactor requires

--- src/cljs_web3_next/utils.cljs
+++ src/cljs_web3_next/utils.cljs
@@ -1,35 +1,36 @@
-(ns cljs-web3-next.utils)
+(ns cljs-web3-next.utils
+  (:require [oops.core :refer [ocall oget]]))
 
 (defn sha3 [provider arg]
-  (js-invoke (aget provider "utils") "sha3" arg))
+  (ocall (oget provider "utils") "sha3" arg))
 
 (defn solidity-sha3 [provider arg & args]
-  (apply js-invoke (aget provider "utils") "soliditySha3" arg args))
+  (ocall (oget provider "utils") "soliditySha3" arg args))
 
 (defn from-ascii [provider arg]
-  (js-invoke (aget provider "utils") "fromAscii" arg))
+  (ocall (oget provider "utils") "fromAscii" arg))
 
 (defn to-ascii [provider arg]
-  (js-invoke (aget provider "utils") "toAscii" arg))
+  (ocall (oget provider "utils") "toAscii" arg))
 
 (defn to-hex
   "Will auto convert any given value to HEX.
   Number strings will interpreted as numbers.
   Text strings will be interpreted as UTF-8 strings."
   [provider arg]
-  (js-invoke (aget provider "utils") "toHex" arg))
+  (ocall (oget provider "utils") "toHex" arg))
 
 (defn number-to-hex [provider number]
-  (js-invoke (aget provider "utils") "numberToHex" number))
+  (ocall (oget provider "utils") "numberToHex" number))
 
 (defn from-wei [provider number & [unit]]
-  (js-invoke (aget provider "utils") "fromWei" (str number) (name unit)))
+  (ocall (oget provider "utils") "fromWei" (str number) (name unit)))
 
 (defn to-wei [provider number & [unit]]
-  (js-invoke (aget provider "utils") "toWei" (str number) (name unit)))
+  (ocall (oget provider "utils") "toWei" (str number) (name unit)))
 
 (defn address? [provider address]
-  (js-invoke (aget provider "utils") "isAddress" address))
+  (ocall (oget provider "utils") "isAddress" address))
 
 (defn address->checksum [provider address]
-  (js-invoke (aget provider "utils") "toChecksumAddress" address))
+  (ocall (oget provider "utils") "toChecksumAddress" address))
