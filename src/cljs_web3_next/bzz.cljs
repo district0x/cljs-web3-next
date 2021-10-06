d98f643ecd7b239138c04de45d12c2d3164c95c5
st4cks1deflow <1@stacksideflow.host>
2021-10-06 14:33:04 +0300

    Alias deprecated bzz fns

--- src/cljs_web3_next/bzz.cljs
+++ src/cljs_web3_next/bzz.cljs
@@ -5,20 +5,15 @@
   (:require [cljs-web3-next.helpers :as web3-helpers]
             [oops.core :refer [ocall oget oset! oapply+]]))
 
-(defn get-bzz [web3]
-  (oget web3 "bzz"))
+(defn get-bzz [provider]
+  (oget provider "bzz"))
 
 (defn block-network-read [provider & args]
-  (js-apply (get-bzz web3) "blockNetworkRead" args))
+  (oapply+ (get-bzz provider) "blockNetworkRead" args))
 
 (defn download [web3 & args]
   (js-apply (get-bzz web3) "download" args))
 
-(defn get [web3 & args]
-  (js-apply (get-bzz web3) "get" args))
-
-(defn put [web3 & args]
-  (js-apply (get-bzz web3) "put" args))
 
 (def hive (u/prop-or-clb-fn "bzz" "hive"))
 (def info (u/prop-or-clb-fn "bzz" "info"))
@@ -30,13 +25,18 @@
   (js-apply (get-bzz web3) "retrieve" args))
 
 (defn store [web3 & args]
-  (js-apply (get-bzz web3) "store" args))
+  (oapply+ (get-bzz web3) "store" args))
 
 (defn upload [web3 & args]
-  (js-apply (get-bzz web3) "upload" args))
+  (oapply+ (get-bzz web3) "upload" args))
 
+;; DEPRECATED
 (defn swap-enabled? [web3 & args]
   (js-apply (get-bzz web3) "swapEnabled" args))
 
+;; DEPRECATED
 (defn sync-enabled? [web3 & args]
-  (js-apply (get-bzz web3) "syncEnabled" args))
+  false)
+
+(def get download)
+(def put upload)
